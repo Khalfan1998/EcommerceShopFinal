d254b1686c32b17148fe8b0516d0aca983d9fe67
Khalfan1998 <khalfan.ikns@gmail.com>
2022-01-27 12:53:25 +0300

    Ionic Capacitor
    adding ionic capacitor to run webapp on android studio emulator

--- client/src/responsive.js
+++ client/src/responsive.js
@@ -2,7 +2,8 @@ import { css } from "styled-components";
 
 export const mobile = (props) => {
   return css`
-    @media only screen and (max-width: 380px) {
+    // @media only screen and (max-width: 380px)
+    @media only screen and (-webkit-min-device-pixel-ratio: 2.625) {
       ${props}
     }
   `;
